87a21d371b7099736aa1c4c4ca586a0e25fe0073
claireporter26 <58444269+claireporter26@users.noreply.github.com>
2020-12-01 16:16:16 +0000

    added timer and score functionality

--- js/main.js
+++ js/main.js
@@ -7,9 +7,25 @@ let chosenCard;
 let chosenCardImg;
 let chosenCard1;
 let chosenCard2;
+let scoreCount = document.querySelector(".score");
 
 
 //2. on click show that cards picture until 2 cards are shown
+
+
+function once(fn, context) { 
+    var result;
+
+    return function() { 
+        if(fn) {
+            result = fn.apply(context || this, arguments);
+            fn = null;
+        }
+
+        return result;
+    };
+}
+
 
 
 cards.forEach(function(card){
@@ -17,18 +33,30 @@ cards.forEach(function(card){
         chosenCard = e.target;
         chosenCardImg = chosenCard.querySelector(".card-img");
 
+
+       startTimer()
+
         
         if(cardCounter < 2){
             chosenCardImg.style.opacity = 1;
             cardCounter++;
             chosenCardImg.classList.add("chosenCard" + cardCounter)
-            console.log(chosenCardImg.classList)
+            console.log(chosenCardImg.dataset)
             
             if(cardCounter >= 2){
                 isMatch()
             }
         }
     })
+})
+
+let startTimer = once(function(){
+    var sec = 0;
+    function pad ( val ) { return val > 9 ? val : "0" + val; }
+    setInterval( function(){
+        document.getElementById("seconds").innerHTML=pad(++sec%60);
+        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+    }, 1000);
 })
 
 //3. check if 2 cards match
@@ -38,10 +66,23 @@ function isMatch(){
 
     //4. if no match stop showing picture, if is match increase counter and leave picture showing
     if(chosenCard1.dataset.foodType === chosenCard2.dataset.foodType){
-        alert("match")
+        
+        matchCounter++;
+        scoreCount.innerHTML = `Score: ${matchCounter} Pairs`
+
+        if(matchCounter == 6){
+            setTimeout(function(){
+                alert("You've won!")
+            }, 1500)
+            
+        }
+
     }else{
-        chosenCard1.style.opacity = 0;
-        chosenCard2.style.opacity = 0;
+        setTimeout(function(){
+            chosenCard1.style.opacity = 0;
+            chosenCard2.style.opacity = 0;
+        }, 1000)
+      
     }
 
     cardCounter = 0;
